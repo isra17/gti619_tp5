cd96f6e57a3e9ade9c298a42577ddd3ffec04786
isra17 <isra017@gmail.com>
2016-04-04 13:09:50 -0400

    Add secure cookie and duration config

--- shapr/config.py
+++ shapr/config.py
@@ -1,8 +1,13 @@
 # Default development config
+from datetime import timedelta
 import os
+
 DEBUG = os.environ.get('DEBUG', '0') == '1'
 SECRET_KEY = os.environ.get('SECRET_KEY', '\x00'*32)
 SQLALCHEMY_DATABASE_URI = \
         os.environ.get('DATABASE_URL', 'sqlite:////tmp/shapr.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PASSWORD_ALGO = 'bcrypt'
+
+SESSION_COOKIE_SECURE = not DEBUG
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=20)
