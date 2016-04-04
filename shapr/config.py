e02dcb8a8587e0d92df853ded10b231ce9640b47
isra17 <isra017@gmail.com>
2016-04-04 12:26:24 -0400

    Load config from environment

--- shapr/config.py
+++ shapr/config.py
@@ -1,7 +1,8 @@
 # Default development config
-DEBUG = False
-SECRET_KEY = '\x00'*32
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/shapr.db'
+import os
+DEBUG = os.environ.get('DEBUG', '0') == '1'
+SECRET_KEY = os.environ.get('SECRET_KEY', '\x00'*32)
+SQLALCHEMY_DATABASE_URI = \
+        os.environ.get('DATABASE_URL', 'sqlite:////tmp/shapr.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PASSWORD_ALGO = 'bcrypt'
